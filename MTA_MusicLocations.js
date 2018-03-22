6a8c121fbc26e0edfb270777d8da63237baaa297
Rushali <rushaliy@gmail.com>
2018-03-22 16:40:33 -0400

    🍩 happy commit. test is functional

--- MTA_MusicLocations.js
+++ MTA_MusicLocations.js
@@ -1,49 +1,50 @@
 var geojson = {
   type : 'FeatureCollection',
-  features: [{
-                type: 'Feature',
-                geometry: {
-                  type: 'Point',
-                  coordinates: [-73.925650, 40.828001]
-                },
-                properties: {
-                  title: 'MUNY',
-                  description: 'Yankee Stadium'
-                }
-              },
-              {
-                type: 'Feature',
-                geometry: {
-                  type: 'Point',
-                  coordinates: [-73.952837, 40.810756]
-                },
-                properties: {
-                  title: 'MUNY',
-                  description: '125th 8th Av'
-                }
-              },
-              {
-                type: 'Feature',
-                geometry: {
-                  type: 'Point',
-                  coordinates: [-73.937221, 40.804403]
-                },
-                properties: {
-                  title: 'MUNY',
-                  description: '125th lexington Av'
-                }
-              },
-              {
-                type: 'Feature',
-                geometry: {
-                  type: 'Point',
-                  coordinates: [-73.950944, 40.785850]
-                },
-                properties: {
-                  title: 'MUNY',
-                  description: '96th 2nd Av'
-                }
-              },
+  features: [
+              // {
+              //   type: 'Feature',
+              //   geometry: {
+              //     type: 'Point',
+              //     coordinates: [-73.925650, 40.828001]
+              //   },
+              //   properties: {
+              //     title: 'MUNY',
+              //     description: 'Yankee Stadium'
+              //   }
+              // },
+              // {
+              //   type: 'Feature',
+              //   geometry: {
+              //     type: 'Point',
+              //     coordinates: [-73.952837, 40.810756]
+              //   },
+              //   properties: {
+              //     title: 'MUNY',
+              //     description: '125th 8th Av'
+              //   }
+              // },
+              // {
+              //   type: 'Feature',
+              //   geometry: {
+              //     type: 'Point',
+              //     coordinates: [-73.937221, 40.804403]
+              //   },
+              //   properties: {
+              //     title: 'MUNY',
+              //     description: '125th lexington Av'
+              //   }
+              // },
+              // {
+              //   type: 'Feature',
+              //   geometry: {
+              //     type: 'Point',
+              //     coordinates: [-73.950944, 40.785850]
+              //   },
+              //   properties: {
+              //     title: 'MUNY',
+              //     description: '96th 2nd Av'
+              //   }
+              // },
               {
                 type: 'Feature',
                 geometry: {
@@ -51,7 +52,7 @@ var geojson = {
                   coordinates: [-73.951473, 40.777881]
                 },
                 properties: {
-                  title: '<video width="320" height="240" controls><source src="/Data/Video/video1.MP4" type="video/mp4"></video>',
+                  title: '<video width="720px" height="480px" top="0" left="0" controls><source src="/Data/Video/video1.MP4" type="video/mp4"></video>',
                   description: '86th 2nd Av'
                 }
               },
@@ -62,7 +63,7 @@ var geojson = {
                   coordinates: [-73.958426, 40.768655]
                 },
                 properties: {
-                  title: '<img src="Data/Images/musician1.jpg" alt="musician in central park with saxophone" width="500" height="600">',
+                  title: '<img src="Data/Images/musician1.jpg" alt="musician in central park with saxophone" width="720px" height="480px" top="0" left="0">',
                   description: '72nd 2nd Av'
                 }
               },
@@ -73,7 +74,8 @@ var geojson = {
                   coordinates: [-73.981807, 40.768484]
                 },
                 properties: {
-                  title: '<iframe src="/dist/vrview-master/examples/video/index.html"></iframe>',
+                  title: '<iframe src="/dist/vrview-master/examples/video/index.html" width="720px" height="500px" top="0" left="0"></iframe>',  //dist/vrview-master/examples/video/index.html
+                  //<iframe width="720" height="480" src="https://www.youtube.com/embed/DBiuVW5aOGc" frameborder="0" allowfullscreen></iframe>
                   description: '59th St - Columbus Circle'
                 }
               },
@@ -84,7 +86,7 @@ var geojson = {
                   coordinates: [-73.967866, 40.762988]
                 },
                 properties: {
-                  title: '<iframe src="underground.html"></iframe>',
+                  title: '<iframe src="underground.html" width="720px" height="480px" top="0" left="0">',
                   description: 'LEXINGTON AVENUE - 59TH STREET'
                 }
               },
@@ -95,7 +97,7 @@ var geojson = {
                   coordinates: [-73.969074, 40.757698]
                 },
                 properties: {
-                  title: 'MUNY',
+                  title: '<iframe src="/dist/vrview-master/examples/video/index.html" width="720px" height="480px" top="0" left="0"></iframe>',
                   description: '53rd STREET - Lexington Avenue'
                 }
               },
@@ -106,7 +108,8 @@ var geojson = {
                   coordinates: [-73.981333, 40.758668]
                 },
                 properties: {
-                  title: 'MUNY',
+                  title: '<iframe src="/iframes/webgl_video_panorama_equirectangular.html" width="720px" height="480px" top="0" left="0" ></iframe>', // examples/webgl_video_panorama_equirectangular.html
+                  // width="'+ window.innerWidth+' "height="' + window.innerHeight+'" top="0" left="0"
                   description: '47-50 Streets - Rockefeller center'
                 }
               },
@@ -117,7 +120,7 @@ var geojson = {
                   coordinates: [-73.989980, 40.757465]
                 },
                 properties: {
-                  title: 'MUNY',
+                  title: '<iframe src="/dist/vrview-master/examples/video/index.html" width="720px" height="500px" top="0" left="0"></iframe>',
                   description: '42ND STREET - PORT AUTHORITY BUS TERMINAL'
                 }
               },
@@ -128,7 +131,7 @@ var geojson = {
                   coordinates: [-73.987277, 40.755238]
                 },
                 properties: {
-                  title: 'MUNY',
+                  title: '<iframe src="/iframes/canvas_materials_video.html" width="720px" height="480px" top="0" left="0" ></iframe>', //canvas_materials_video.html
                   description: 'TIMES SQUARE - 42ND STREET'
                 }
               },
@@ -154,127 +157,127 @@ var geojson = {
                   description: '34th STREET - HERALD SQUARE'
                 }
               },
-              {
-                type: 'Feature',
-                geometry: {
-                  type: 'Point',
-                  coordinates: [-73.983865, 40.743323]
-                },
-                properties: {
-                  title: 'MUNY',
-                  description: '28th STREET, Lexington Avenue'
-                }
-              },
-              {
-                type: 'Feature',
-                geometry: {
-                  type: 'Point',
-                  coordinates: [-73.991226, 40.735015]
-                },
-                properties: {
-                  title: 'MUNY',
-                  description: '14th STREET-UNION SQUARE'
-                }
-              },
-              {
-                type: 'Feature',
-                geometry: {
-                  type: 'Point',
-                  coordinates: [-73.990860, 40.730104]
-                },
-                properties: {
-                  title: 'MUNY',
-                  description: 'ASTOR PLACE'
-                }
-              },
-              {
-                type: 'Feature',
-                geometry: {
-                  type: 'Point',
-                  coordinates: [-74.008327, 40.709385]
-                },
-                properties: {
-                  title: 'MUNY',
-                  description: 'Fulton Street'
-                }
-              },
-              {
-                type: 'Feature',
-                geometry: {
-                  type: 'Point',
-                  coordinates: [-74.009078, 40.710573]
-                },
-                properties: {
-                  title: 'MUNY',
-                  description: 'Fulton Center'
-                }
-              },
-              {
-                type: 'Feature',
-                geometry: {
-                  type: 'Point',
-                  coordinates: [-74.013544, 40.704647]
-                },
-                properties: {
-                  title: 'MUNY',
-                  description: 'BOWLING GREEN'
-                }
-              },
-              {
-                type: 'Feature',
-                geometry: {
-                  type: 'Point',
-                  coordinates: [-73.990130, 40.693834]
-                },
-                properties: {
-                  title: 'MUNY',
-                  description: 'Court Street & Borough Hall'
-                }
-              },
-              {
-                type: 'Feature',
-                geometry: {
-                  type: 'Point',
-                  coordinates: [-73.978372, 40.684462]
-                },
-                properties: {
-                  title: 'MUNY',
-                  description: 'ATLANTIC AVENUE - BARCLAYS CENTER'
-                }
-              },
-              {
-                type: 'Feature',
-                geometry: {
-                  type: 'Point',
-                  coordinates: [-73.971055, 40.675235]
-                },
-                properties: {
-                  title: 'MUNY',
-                  description: 'GRAND ARMY PLAZA'
-                }
-              },
-              {
-                type: 'Feature',
-                geometry: {
-                  type: 'Point',
-                  coordinates: [-73.890858,40.746689]
-                },
-                properties: {
-                  title: 'MUNY',
-                  description: 'JACKSON HEIGHTS - ROOSEVELT AVENUE'
-                }
-              },
-              {
-                type: 'Feature',
-                geometry: {
-                  type: 'Point',
-                  coordinates: [-73.843700, 40.752390]
-                },
-                properties: {
-                  title: 'MUNY',
-                  description: 'METS - WILLETS AVENUE'
-                }
-              }
+              // {
+              //   type: 'Feature',
+              //   geometry: {
+              //     type: 'Point',
+              //     coordinates: [-73.983865, 40.743323]
+              //   },
+              //   properties: {
+              //     title: 'MUNY',
+              //     description: '28th STREET, Lexington Avenue'
+              //   }
+              // },
+              // {
+              //   type: 'Feature',
+              //   geometry: {
+              //     type: 'Point',
+              //     coordinates: [-73.991226, 40.735015]
+              //   },
+              //   properties: {
+              //     title: 'MUNY',
+              //     description: '14th STREET-UNION SQUARE'
+              //   }
+              // },
+              // {
+              //   type: 'Feature',
+              //   geometry: {
+              //     type: 'Point',
+              //     coordinates: [-73.990860, 40.730104]
+              //   },
+              //   properties: {
+              //     title: 'MUNY',
+              //     description: 'ASTOR PLACE'
+              //   }
+              // },
+              // {
+              //   type: 'Feature',
+              //   geometry: {
+              //     type: 'Point',
+              //     coordinates: [-74.008327, 40.709385]
+              //   },
+              //   properties: {
+              //     title: 'MUNY',
+              //     description: 'Fulton Street'
+              //   }
+              // },
+              // {
+              //   type: 'Feature',
+              //   geometry: {
+              //     type: 'Point',
+              //     coordinates: [-74.009078, 40.710573]
+              //   },
+              //   properties: {
+              //     title: 'MUNY',
+              //     description: 'Fulton Center'
+              //   }
+              // },
+              // {
+              //   type: 'Feature',
+              //   geometry: {
+              //     type: 'Point',
+              //     coordinates: [-74.013544, 40.704647]
+              //   },
+              //   properties: {
+              //     title: 'MUNY',
+              //     description: 'BOWLING GREEN'
+              //   }
+              // },
+              // {
+              //   type: 'Feature',
+              //   geometry: {
+              //     type: 'Point',
+              //     coordinates: [-73.990130, 40.693834]
+              //   },
+              //   properties: {
+              //     title: 'MUNY',
+              //     description: 'Court Street & Borough Hall'
+              //   }
+              // },
+              // {
+              //   type: 'Feature',
+              //   geometry: {
+              //     type: 'Point',
+              //     coordinates: [-73.978372, 40.684462]
+              //   },
+              //   properties: {
+              //     title: 'MUNY',
+              //     description: 'ATLANTIC AVENUE - BARCLAYS CENTER'
+              //   }
+              // },
+              // {
+              //   type: 'Feature',
+              //   geometry: {
+              //     type: 'Point',
+              //     coordinates: [-73.971055, 40.675235]
+              //   },
+              //   properties: {
+              //     title: 'MUNY',
+              //     description: 'GRAND ARMY PLAZA'
+              //   }
+              // },
+              // {
+              //   type: 'Feature',
+              //   geometry: {
+              //     type: 'Point',
+              //     coordinates: [-73.890858,40.746689]
+              //   },
+              //   properties: {
+              //     title: 'MUNY',
+              //     description: 'JACKSON HEIGHTS - ROOSEVELT AVENUE'
+              //   }
+              // },
+              // {
+              //   type: 'Feature',
+              //   geometry: {
+              //     type: 'Point',
+              //     coordinates: [-73.843700, 40.752390]
+              //   },
+              //   properties: {
+              //     title: 'MUNY',
+              //     description: 'METS - WILLETS AVENUE'
+              //   }
+               //   }
 
             ]
 };
