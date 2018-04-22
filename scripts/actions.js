17e32fa55d6baf367571a4d60531396d884e52d8
Rushali <rushaliy@gmail.com>
2018-04-22 13:46:26 -0400

    styling problems

--- scripts/actions.js
+++ scripts/actions.js
@@ -17,26 +17,26 @@ $(document).ready(function() {
         // adding the content based on the clicked element ID
         switch (element.target.id) {
             case '86th_2nd_Av':
-                $('.media_cover').append('<iframe src="../iframes/webgl_video2.html" style="position:fixed; margin: auto; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; "></iframe>');
+                $('.media_cover').append('<iframe src="" style="position:fixed; margin: auto; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; "></iframe>');
                 break;
             case '72nd_2nd_Av':
-                $('.media_cover').append('<iframe src="../iframes/underground.html" style="position:fixed; margin: auto; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; "></iframe>');
+                $('.media_cover').append('<iframe src="" style="position:fixed; margin: auto; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; "></iframe>');
                 break;
             case '59th_St_-_Columbus_Circle':
-                $('.media_cover').append('<iframe src="../iframes/webgl_video_panorama_equirectangular.html" style="position:fixed; margin: auto; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; "></iframe>');
+                $('.media_cover').append('<iframe src="" style="position:fixed; margin: auto; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; "></iframe>');
                 break;
             case 'LEXINGTON_AVENUE_-_59TH STREET':
-                $('.media_cover').append('<img src="../Data/Images/musician1.jpg" alt="musician in central park with saxophone" style="position:fixed; margin: auto; top:0px; left:0px; bottom:0px; right:0px; height:100%; border:none; padding:0; overflow:hidden;"> ');
+                $('.media_cover').append('<img src="" alt="" style="position:fixed; margin: auto; top:0px; left:0px; bottom:0px; right:0px; height:100%; border:none; padding:0; overflow:hidden;"> ');
                 break;
             case '53rd_STREET_-_Lexington_Avenue':
-                $('.media_cover').append('<iframe src="../iframes/canvas_materials_video.html" style="position:fixed; margin: auto; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; "></iframe>');
+                $('.media_cover').append('<iframe src="" style="position:fixed; margin: auto; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; "></iframe>');
                 break;
             case '47-50_Streets_-_Rockefeller_center':
                 $('.media_cover').append('<iframe width="1120" height="630" src="https://www.youtube.com/embed/kszWN8TteIE" style="frameborder="0" allow="autoplay; encrypted-media" allowfullscreen position:fixed; margin: auto; width:100%; height:100%; overflow:hidden; "></iframe>');
                 //<iframe width="560" height="315" src="https://www.youtube.com/embed/kszWN8TteIE" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe> top:0px; left:0px; bottom:0px; right:0px; border:none; margin:0; padding:0;
                 break;
             case '42ND_STREET_-_PORT_AUTHORITY_BUS_TERMINAL':
-                $('.media_cover').append('<iframe src="../iframes/underground.html" style="position:fixed; margin: auto; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; "></iframe>');
+                $('.media_cover').append('<iframe src="" style="position:fixed; margin: auto; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; "></iframe>');
                 break;
             case 'TIMES_SQUARE_-_42ND_STREET':
                 $('.media_cover').append('<iframe src="" style="position:fixed; margin: auto; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; "></iframe>');
@@ -48,7 +48,7 @@ $(document).ready(function() {
                 $('.media_cover').append('<iframe src="" style="position:fixed; margin: auto; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; "></iframe>');
                 break;
             case '14th_STREET-UNION_SQUARE':
-                $('.media_cover').append('<iframe src="../iframes/webgl_video2.html" style="position:fixed; margin: auto; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; "></iframe>');
+                $('.media_cover').append('<iframe src="" style="position:fixed; margin: auto; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; "></iframe>');
                 break;
             default:
                 console.log("❌ I'm sorry, I don't recognize this marker.")
@@ -88,19 +88,20 @@ $(document).ready(function() {
         });
     });
 
-    var gifs = ['/Data/Images/drum.gif',
-            '/Data/Images/mic.gif',
-            '/Data/Images/guitar.gif'];
+    // var gifs = ['/Data/Images/drum.gif',
+    //         '/Data/Images/mic.gif',
+    //         '/Data/Images/guitar.gif'];
 
-    var randomNumber = Math.floor(Math.random() * gifs.length);
-    var classToAdd = gifs[randomNumber];
+    // var randomNumber = Math.floor(Math.random() * gifs.length);
+    // var classToAdd = gifs[randomNumber];
 
-$('.spottedmarker').addClass(classToAdd);
+//$('.spottedmarker').addClass(classToAdd);
 
     function add_close_listener() {
         markerClicked = false;
         //console.log(markerClicked);
         $('.about_cover').hide();
+      
         $('.media_cover').hide();
         $('.media_text').remove();
         $('.about_button').show();
